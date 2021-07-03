9df10054ceb330ef9a2c1ee6c4901a724228e1bc
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-07-03 15:06:22 +0200

    add showMore/less functionality
    uses the showMore() to hide and show more information about a project

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function showMore() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("readMore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
